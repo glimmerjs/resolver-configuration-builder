a4c467b514f8b84240f4772109f77d3a4909260a
Robert Jackson <rjackson@linkedin.com>
2017-04-08 08:24:02 -0400

    Refactor tests to use `co` for simpler async...

--- test/resolver-configuration-builder-test.js
+++ test/resolver-configuration-builder-test.js
@@ -3,48 +3,48 @@
 const ResolverConfigurationBuilder = require('..');
 const { build } = require('broccoli-fixture');
 const path = require('path');
+const co = require('co');
 const assert = require('assert');
 
 describe('resolver-configuration-builder', function() {
-  it('can read a config file and log results (if requested)', function() {
+  it('can read a config file and log results (if requested)', co.wrap(function* () {
     let config = path.join(process.cwd(), 'test', 'fixtures', 'config');
     let options = { configPath: 'environment.json', logResult: true };
 
     let configBuilder = new ResolverConfigurationBuilder(config, options);
 
-    return build(configBuilder)
-      .then(() => {
-        assert.deepEqual(
-          configBuilder.result,
-          {
-            "app": { "name": 'my-app', "rootName": 'my-app' },
-            "types": {
-              "application": { "definitiveCollection": "main" },
-              "component": { "definitiveCollection": "components" },
-              "renderer": { "definitiveCollection": "main" },
-              "template": { "definitiveCollection": "components" },
-              "util": { "definitiveCollection": "utils" }
-            },
-            "collections": {
-              "main": {
-                "types": ["application", "renderer"]
-              },
-              "components": {
-                "group": "ui",
-                "types": ["component", "template"],
-                "defaultType": "component",
-                "privateCollections": ["utils"]
-              },
-              "utils": {
-                "unresolvable": true
-              }
-            }
-          }
-        );
-      });
-  });
+    yield build(configBuilder);
 
-  it('can use config options if no config file exists', function() {
+    assert.deepEqual(
+      configBuilder.result,
+      {
+        "app": { "name": 'my-app', "rootName": 'my-app' },
+        "types": {
+          "application": { "definitiveCollection": "main" },
+          "component": { "definitiveCollection": "components" },
+          "renderer": { "definitiveCollection": "main" },
+          "template": { "definitiveCollection": "components" },
+          "util": { "definitiveCollection": "utils" }
+        },
+        "collections": {
+          "main": {
+                "types": ["application", "renderer"]
+          },
+          "components": {
+            "group": "ui",
+            "types": ["component", "template"],
+            "defaultType": "component",
+            "privateCollections": ["utils"]
+          },
+          "utils": {
+            "unresolvable": true
+          }
+        }
+      }
+    );
+  }));
+
+  it('can use config options if no config file exists', co.wrap(function* () {
     let config = path.join(process.cwd(), 'test', 'fixtures', 'config');
 
     let options = {
@@ -78,35 +78,34 @@ describe('resolver-configuration-builder', function() {
 
     let configBuilder = new ResolverConfigurationBuilder(config, options);
 
-    return build(configBuilder)
-      .then(() => {
-        assert.deepEqual(
-          configBuilder.result,
-          {
-            "app": { "name": 'my-app', "rootName": 'my-app' },
-            "types": {
-              "application": { "definitiveCollection": "main" },
-              "component": { "definitiveCollection": "components" },
-              "renderer": { "definitiveCollection": "main" },
-              "template": { "definitiveCollection": "components" },
-              "util": { "definitiveCollection": "utils" }
-            },
-            "collections": {
-              "main": {
-                "types": ["application", "renderer"]
-              },
-              "components": {
-                "group": "ui",
-                "types": ["component", "template"],
-                "defaultType": "component",
-                "privateCollections": ["utils"]
-              },
-              "utils": {
-                "unresolvable": true
-              }
-            }
+    yield build(configBuilder);
+
+    assert.deepEqual(
+      configBuilder.result,
+      {
+        "app": { "name": 'my-app', "rootName": 'my-app' },
+        "types": {
+          "application": { "definitiveCollection": "main" },
+          "component": { "definitiveCollection": "components" },
+          "renderer": { "definitiveCollection": "main" },
+          "template": { "definitiveCollection": "components" },
+          "util": { "definitiveCollection": "utils" }
+        },
+        "collections": {
+          "main": {
+            "types": ["application", "renderer"]
+          },
+          "components": {
+            "group": "ui",
+            "types": ["component", "template"],
+            "defaultType": "component",
+            "privateCollections": ["utils"]
+          },
+          "utils": {
+            "unresolvable": true
           }
-        );
-      });
-  });
+        }
+      }
+    );
+  }));
 });
